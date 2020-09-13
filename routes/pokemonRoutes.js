9b8ba0325bb1606787631310be8ff32530cddf50
anjali-wq <10523771@mydbs.ie>
2020-09-13 15:42:27 +0500

    pokemon description

--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -12,7 +12,19 @@ router.get('/:_name', (req, res) => {
         if (response.statusCode == '200') {
             let mainBody = JSON.parse(response.body)
             let pokemonID = mainBody.id;
-
+            request('https://pokeapi.co/api/v2/pokemon-species/' + pokemonID, (err, r) => {
+                if (r.statusCode == '200') {
+                    let childBody = JSON.parse(r.body);
+                    childBody.flavor_text_entries.some(flavour => {
+                        if (flavour.language.name === 'en') {
+                            pokemonDescription = flavour.flavor_text
+                        }
+                    })
+                }
+                else {
+                    res.json({ Error: "Error while sending request!" })
+                }
+            })
         } else {
             res.json({ Error: "Error while sending request!" })
         }
